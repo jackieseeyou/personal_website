1882f4cff201a0a8d139e4fd08f7e0d8c0d37329
jackieseeyou <121642950+jackieseeyou@users.noreply.github.com>
2024-12-31 17:38:51 +1300

    create NavBar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@
 import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './Pages/Home/NavBar';
 import Home from './Pages/Home/HomeScreen';
+
 
 function App() {
   return (
     <div className="App"> 
-      <Home/>
+      <Router>
+        <div>
+          <Navbar/>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+          </Routes>
+
+          <Routes>
+              <Route path="*" element={<div>404 Not Found</div>}/>
+          </Routes>
+          
+
+        </div>
+      </Router>
+
     </div>
   );
 }
--- src/Pages/Home/NavBar.jsx
+++ src/Pages/Home/NavBar.jsx
@@ -1,6 +1,125 @@
-import {useState, useEffect} from React;
+import {useState, useEffect} from "react";
 import {Link} from 'react-scroll';
 
-function NavBar() {
-    const [show, handleShow] = useState(false);
+function Navbar() {
+    const [navActive, setNavActive] = useState(false);
+    
+    const toggleNav = () => {
+        setNavActive(!navActive);
+    }
+
+    const closeMenu = () => {
+        setNavActive(false);
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 500) {
+                closeMenu();
+            }
+        }
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    useEffect(() => {
+        if(window.innerWidth <= 1200) {
+            closeMenu();
+        }
+    }, []);
+
+    return (
+        <nav className={`navbar ${navActive? "active" : ""}`}>
+            <div> 
+                <img src="../img/logo.svg" alt="Logoipsum"/>
+            </div>
+            <a 
+                className={`nav_hamburger ${navActive? 
+                "active" : ""}`} 
+                onClick={toggleNav}
+            >
+                <span className="nav_hamburger__line"></span>
+                <span className="nav_hamburger__line"></span>
+                <span className="nav_hamburger__line"></span>
+            </a>
+            <div className={`navbar--items ${navActive? "active" : ""}`}>
+                <ul>
+                    <li>
+                        <Link 
+                            onClick={closeMenu}
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="heroSection"
+                            className="navbar--content"
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            onClick={closeMenu}
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="MyPortfolio"
+                            className="navbar--content"
+                        >
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            onClick={closeMenu}
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="AboutMe"
+                            className="navbar--content"
+                        >
+                            About Me
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            onClick={closeMenu}
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="Testimonials"
+                            className="navbar--content"
+                        >
+                            Testimonals
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            <Link onClick={closeMenu}
+                activeClass="navbar--active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="Contact"
+                className="btn btn-outline-primary"
+            >
+                Contact Me
+            </Link>
+        </nav>
+    );
+    
 }
+
+export default Navbar;
