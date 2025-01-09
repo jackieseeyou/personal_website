86d2545c4796f6f6a0523f3950ac94d649dd04d6
jackieseeyou <121642950+jackieseeyou@users.noreply.github.com>
2025-01-09 16:33:04 +1300

    add ContactMe and Footer section

--- src/Pages/Home/HomeScreen/index.jsx
+++ src/Pages/Home/HomeScreen/index.jsx
@@ -3,6 +3,8 @@ import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
 import AboutMe from "../AboutMe";
 import MyPortfolio from "../MyPortfolio";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
 
 export default function Home() {
     return  (
@@ -11,6 +13,8 @@ export default function Home() {
          <MySkills/>
          <AboutMe/>
          <MyPortfolio/>
+         <ContactMe/>
+         <Footer></Footer>
         </>
     )
 }
