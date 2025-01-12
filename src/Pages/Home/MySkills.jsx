76bce4e6ea1a2ad36f080fbefa2c128d4caab7aa
jackieseeyou <121642950+jackieseeyou@users.noreply.github.com>
2025-01-13 12:35:11 +1300

    change mySkills to techstack

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -4,26 +4,21 @@ export default function MySkills() {
     return (
         <section className="skills--section" id="mySkills"> 
             <div className="portfolio--container">
-                <p className="section--title">My Skills</p>
-                <h2 className="skills--section--heading">My Expertise</h2>
+                <p className="section--title">Technical Proficiencies</p>
+                <h2 className="skills--section--heading">My Skills</h2>
             </div>
-            <div className="skills--section--container">
-                {data?.skills?.map((item, index) => (
-                    <div 
-                        key={index}
-                        className="skills--section--card"
-                    >
-                        <div className="skills--section--img">
-                            <img src={item.src} alt="Product Chain"/>
-                        </div>
 
-                        <div className="skills--section--card--content">
-                            <h3 clasName="skills--section--title">{item.title}</h3>
-                            {/* <p clasName="skills--section--description">{item.description}</p> */}
+
+            <div className="skills--section--container">
+                <div className="skills--section--img--container">
+                    {data?.skills?.map((item, index) => (
+                        <div key={index} className="skills--section--img">
+                            <img src={item.src} alt={item.name}/>
                         </div>
-                    </div>
                     ))}
+                </div>
             </div>
+
         </section>
     )
 }
