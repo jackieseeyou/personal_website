e75d063d7a4e8b7d5d392671e521fd01ab830b18
jackieseeyou <121642950+jackieseeyou@users.noreply.github.com>
2025-10-01 00:53:16 +1300

    change project links for public and private repos

--- src/Pages/Home/MyProjects.jsx
+++ src/Pages/Home/MyProjects.jsx
@@ -44,22 +44,28 @@ export default function MyPortfolio() {
                             </ul>
 
                             <p className="text-sm portfolio--link">
-                                
-                                <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                viewBox="0 0 20 19"
-                                fill="none"
-                                >
-                                <path
-                                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
-                                    stroke="currentColor"
-                                    stroke-width="2.66667"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                                </svg>
+                                {item.link ? (
+                                    <a href={item.link} target="_blank" rel="noopener noreferrer">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        viewBox="0 0 20 19"
+                                        fill="none"
+                                        style={{ color: "#808080" }} 
+                                    >
+                                        <path
+                                        d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                                        stroke="currentColor"
+                                        strokeWidth="2.66667"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        />
+                                    </svg>
+                                    </a>
+                                ) : (
+                                    <span>Private repo. Available upon request.</span>
+                                )}
                             </p>
                         </div>
                         
